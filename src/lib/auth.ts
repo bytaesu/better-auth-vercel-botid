16b8551be7a65ef7ac41969f0a97a3805bbea964
Taesu <bytaesu@gmail.com>
2026-09-24 08:00:28 +0900

    no-botid

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,7 @@
 import { betterAuth } from "better-auth";
 import { nextCookies } from "better-auth/next-js";
-import { captcha } from "better-auth/plugins";
-import { checkBotId } from "botid/server";
+// import { captcha } from "better-auth/plugins";
+// import { checkBotId } from "botid/server";
 import { Pool } from "pg";
 
 export const auth = betterAuth({
@@ -12,11 +12,11 @@ export const auth = betterAuth({
     enabled: true,
   },
   plugins: [
-    captcha({
-      provider: "vercel-botid",
-      endpoints: ["/sign-up/email", "/sign-in/email"],
-      checkBotId,
-    }),
+    // captcha({
+    //   provider: "vercel-botid",
+    //   endpoints: ["/sign-up/email", "/sign-in/email"],
+    //   checkBotId,
+    // }),
     nextCookies()
   ],
 });
